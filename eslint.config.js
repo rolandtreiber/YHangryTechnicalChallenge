78539de32ff9a4401fa9b9c84a3072f16c74a440
Roland Treiber <rolandtreiber@gmail.com>
2025-03-17 14:56:34 +0000

    Linter

--- eslint.config.js
+++ eslint.config.js
@@ -34,7 +34,9 @@ export default [
         },
         rules: {
             'react-hooks/rules-of-hooks': 'error',
-            'react-hooks/exhaustive-deps': 'warn',
+            'react-hooks/exhaustive-deps': 'off',
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": "off"
         },
     },
     {
